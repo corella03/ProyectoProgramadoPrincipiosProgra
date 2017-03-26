977122574bb1976e03b19206656350d762218cc8
corella03 <lcorellac@ucatolica.ac.cr>
2017-03-25 19:59:07 -0600

    Modificando el Login

--- Administrador/Login.py
+++ Administrador/Login.py
@@ -1,11 +1,14 @@
-def DatosAdministrador():
-    ID = "alonso"
-    contraseña = "123"
-    return ID,contraseña
-def Login(ID, contraseña):
-    ingresoId = input("Digite Usuario")
-    ingresoContra = input("Digite Contra")
-    if ingresoId == ID and ingresoContra == contraseña:
-        print("Bienvenido Mae")
-    else:
-        print("NO ERES BIENVENIDO")
+class Administrador:
+    def __init__(self):
+        self.ID = "alonso"
+        self.contraseña = "123"
+    def  DatosAdministrador(self):
+        ingresoId = input("Digite Usuario")
+        ingresoContra = input("Digite Contra")
+        if ingresoId == self.ID and ingresoContra == self.contraseña:
+            print("Bienvenido ADMIN \n")
+        else:
+            print("NO ERES BIENVENIDO")
+    def cambioContraseña(self,):
+        ingresoNuevaContraseña = input("Ingrese la nueva Contraseña")
+        self.contraseña = ingresoNuevaContraseña
--- Menu/MenuDelSistema.py
+++ Menu/MenuDelSistema.py
@@ -1,10 +1,12 @@
-
-#
 def menu():
-    print("\nSelecciona una opción")
-    print("\t1 - Modo Administrador")
-    print("\t2 - Modo Usuario")
-    print("\t0 - Salir")
+    print("\nSelecciona una opción\n"
+          "\t1 - Modo Administrador\n"
+          "\t2 - Modo Usuario\n"
+          "\t0 - Salir")
+def menuAdmin():
+    print("\nSelecciona una opción\n"
+          "\t1 - Cambiar contra\n"
+          "\t2 - Salir\n")
 def escogerOpciones():
     while True:
         # Llamamos al Menú
@@ -13,13 +15,12 @@ def escogerOpciones():
         opcionesMenu = input("Ingrese un número del Menú")
         if opcionesMenu == "1":
             import Administrador.Login
-            id,contra = Administrador.Login.DatosAdministrador()
-            Administrador.Login.Login(id, contra)
+            Administrador.Login.Administrador().DatosAdministrador()
             input("\npulsa una tecla para continuar")
         elif opcionesMenu == "2":
             print("")
-            input("Has pulsado la opción 2...\npulsa una tecla para continuar")
-        elif opcionesMenu == "4":
+            input("\npulsa una tecla para continuar")
+        elif opcionesMenu == "0":
             break
         else:
             print("")
